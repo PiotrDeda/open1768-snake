2e716c229db69b167c619a3f8a0a33ad8795c1ca
PiotrDeda <piotrdeda.pl@gmail.com>
2022-01-19 22:46:46 +0100

    Snake

--- lcdDrawing.h
+++ lcdDrawing.h
@@ -3,7 +3,6 @@
 
 #include "Open1768_LCD.h"
 #include "LCD_ILI9325.h"
-#include "asciiLib.h"
 
 void reset_ram()
 {
@@ -19,10 +18,10 @@ void reset_ram()
 void draw_rectangle(int pos_x, int pos_y, int size_x, int size_y, int color)
 {
 	lcdWriteReg(HADRPOS_RAM_START, pos_x);
-	lcdWriteReg(HADRPOS_RAM_END, pos_x + size_x);
+	lcdWriteReg(HADRPOS_RAM_END, pos_x + size_x - 1);
 	
 	lcdWriteReg(VADRPOS_RAM_START, pos_y);
-	lcdWriteReg(VADRPOS_RAM_END, pos_y + size_y);
+	lcdWriteReg(VADRPOS_RAM_END, pos_y + size_y - 1);
 	
 	lcdSetCursor(pos_x, pos_y);
 	lcdWriteIndex(DATA_RAM);
@@ -38,31 +37,6 @@ void draw_point(int x, int y, int color)
 {
 	lcdSetCursor(LCD_MAX_X - x, LCD_MAX_Y - y);
 	lcdWriteReg(DATA_RAM, color);
-}
-
-void draw_char(int x, int y, char c, int font)
-{
-	unsigned char character[16];
-	GetASCIICode(font, character, c);
-	
-	for (int i = 0; i < 16; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			if (((character[i] >> j) & 1) == 1)
-				draw_point(x + i, LCD_MAX_Y - 8 - y + j, LCDBlack);
-		}
-	}
-}
-
-void draw_string(int x, int y, char* str, int font)
-{
-	int i = 0;
-	while(str[i] != '\0')
-	{
-		draw_char(x, y + i * 8, str[i], font);
-		i++;
-	}
 }
 
 void fill_background(int color)
--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 #include "lcdDrawing.h"
 #include "lcdTouch.h"
+#include "sound.h"
+#include "win.h"
+//#include "lose.h"
 
 // Cell:     Apple:
 // #########         /|\ /|\ Apple offset
@@ -21,13 +24,13 @@
 #define GRID_SIZE_Y (LCD_MAX_Y / 8)
 
 enum MovementDirection {LEFT, RIGHT, UP, DOWN};
-
-int snake_head_x = lcd_to_snake(LCD_MAX_X / 2);
-int snake_head_y = lcd_to_snake(LCD_MAX_Y / 2);
+int snake_head_x = (LCD_MAX_X / 2) / CELL_SIZE;
+int snake_head_y = (LCD_MAX_Y / 2) / CELL_SIZE;
 int snake_size = 5;
+short grid[GRID_SIZE_X][GRID_SIZE_Y];
 
 // Convert LCD coords to Snake coords
-int lcd_to_snake(int x, int *y)
+int lcd_to_snake(int x)
 {
 	return x / CELL_SIZE;
 }
@@ -53,29 +56,39 @@ void draw_apple(int x, int y)
 void config_Timer()
 {
 	LPC_TIM0->PR = 0;
-	LPC_TIM0->MR0 = SystemCoreClock / 8000;
+	LPC_TIM0->MR0 = SystemCoreClock / 16;
 	LPC_TIM0->MCR = 3;
 	LPC_TIM0->TCR = 1;
 	NVIC_EnableIRQ(TIMER0_IRQn);
 }
 
-void move_snake(MovementDirection direction)
+void move_snake(enum MovementDirection direction)
 {
+	int target_x;
+	int target_y;
 	switch (direction)
 	{
 		case LEFT:
-			// Moving left
+			target_x = snake_head_x - 1;
+			target_y = snake_head_y;
 			break;
 		case RIGHT:
-			// Moving right
+			target_x = snake_head_x + 1;
+			target_y = snake_head_y;
 			break;
 		case UP:
-			// Moving up
+			target_x = snake_head_x;
+			target_y = snake_head_y + 1;
 			break;
 		case DOWN:
-			// Moving down
+			target_x = snake_head_x;
+			target_y = snake_head_y - 1;
 			break;
 	}
+	
+	grid[target_x][target_y] = snake_size;
+	snake_head_x = target_x;
+	snake_head_y = target_y;
 	
 	//if jabłko zebrane
 	//{
@@ -95,38 +108,94 @@ void move_snake(MovementDirection direction)
 	for (int i = 0; i < GRID_SIZE_X; i++)
 		for (int j = 0; j < GRID_SIZE_Y; j++)
 			if (grid[i][j] > 0)
-				draw_snake_part(i, j)
+				draw_snake_part(i, j);
 	//wygeneruj i narysuj nowe jabłko
+	//play_sound(win, WIN_LEN);
+	//play_sound(lose, LOSE_LEN);
 }
 
 int touch_x, touch_y;
-
+enum MovementDirection direction = LEFT;
+#define SAMPLE_SIZE_INGAME 8
 void TIMER0_IRQHandler(void)
 {
-	touch_x = last_touch_x;
-	touch_y = last_touch_y;
-	touch_to_lcd(&touch_x, &touch_y);
-	touch_x = lcd_to_snake(touch_x);
-	touch_y = lcd_to_snake(touch_y);
+	int *x;
+	int *y;
+	int sample_x, sample_y;
+	*x = 0;
+	*y = 0;
+	for (int i = 0; i < SAMPLE_SIZE_INGAME; i++)
+	{
+		touchpanelGetXY(&sample_x, &sample_y);
+		*x += sample_x;
+		*y += sample_y;
+	}
+	*x /= SAMPLE_SIZE_INGAME;
+	*y /= SAMPLE_SIZE_INGAME;
+	if (*x > 100 && *x < 3500)
+	{
+		
+	touch_x = *x;
+	//touch_x = last_touch_x;
+	//touch_y = last_touch_y;
+	//touch_to_lcd(&touch_x, &touch_y);
+	//touch_x = lcd_to_snake(touch_x);
+	//touch_y = lcd_to_snake(touch_y);
 	
-	MovementDirection direction;
-	if (touch_x < snake_head_x && touch_y > snake_head_y - touch_x && touch_y < snake_head_y + touch_x)
-		direction = LEFT;
-	else if (touch_x > snake_head_x && touch_y > snake_head_y - (touch_x - snake_head_x) && touch_y < snake_head_y + (touch_x - snake_head_x))
-		direction = RIGHT;
-	else if (touch_y < snake_head_y)
-		direction = UP;
-	else
-		direction = DOWN;
-	move_snake(direction);
+	//if (touch_x < snake_head_x && touch_y > snake_head_y - touch_x && touch_y < snake_head_y + touch_x)
+	//	direction = LEFT;
+	//else if (touch_x > snake_head_x && touch_y > snake_head_y - (touch_x - snake_head_x) && touch_y < snake_head_y + (touch_x - snake_head_x))
+	//	direction = RIGHT;
+	//else if (touch_y < snake_head_y)
+	//	direction = UP;
+	//else
+	//	direction = DOWN;
+//	move_snake(direction);
+	
+	switch (direction){
+		case LEFT : 
+		{
+			if (touch_x <= 2000/2)
+				direction = UP;
+			else
+				direction = DOWN;
+			break;
+		}
+		case RIGHT :
+		{
+			if (touch_x <= 2000/2)
+				direction = DOWN;
+			else
+				direction = UP;
+			break;
+		}
+		case UP :
+		{
+			if (touch_x <= 2000/2)
+				direction = RIGHT;
+			else
+				direction = LEFT;
+			break;
+		}
+		case DOWN :
+		{
+			if (touch_x <= 2000/2)
+				direction = LEFT;
+			else
+				direction = RIGHT;
+			break;
+		}
+	}
+	}
+	move_snake (direction);
 	
 	LPC_TIM0->IR = 1;
 }
 
-short grid[GRID_SIZE_X][GRID_SIZE_Y];
-
 int main()
 { 
+	config_GPIOINT();
+	config_sound();
 	lcdConfiguration();
 	init_ILI9325();
 	touchpanelInit();
@@ -137,13 +206,15 @@ int main()
 			grid[i][j] = 0;
 	
 	// Starting snake position
-	grid[5][5] = 5;
-	grid[5][6] = 4;
-	grid[5][7] = 3;
-	grid[5][8] = 2;
-	grid[5][9] = 1;
+	grid[snake_head_x][snake_head_y] = 5;
+	grid[snake_head_x][snake_head_y + 1] = 4;
+	grid[snake_head_x][snake_head_y + 2] = 3;
+	grid[snake_head_x][snake_head_y + 3] = 2;
+	grid[snake_head_x][snake_head_y + 4] = 1;
 	
 	config_Timer();
 	
+	//play_sound(win, WIN_LEN);
+	//play_sound(lose, LOSE_LEN);
 	while(1);
 }
